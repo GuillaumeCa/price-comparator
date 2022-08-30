9862efe64c060ae6205afd0069e778045c11bc41
GuillaumeCa <guicarre1@gmail.com>
2022-08-30 23:26:41 +0200

    fix name

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export function BaseLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="p-7 text-gray-300 max-w-4xl m-auto">
+      <Head>
+        <title>Price Comparator</title>
+      </Head>
+
       <Link href="/">
         <a>
           <h1 className="mb-6 font-bold text-4xl">Price Comparator</h1>
