323719d5d7d1a2d5d5f35d4fcab5914cca253a30
GuillaumeCa <guicarre1@gmail.com>
2025-04-15 21:28:08 +0200

    fix db url

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,7 @@ import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
 
 const client = createClient({
-  url: "file:pricecomparator.db",
+  url: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle({
