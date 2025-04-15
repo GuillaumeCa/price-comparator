06ebd22ebe73c2729ad67200073dc5ea27e1ddf2
GuillaumeCa <guicarre1@gmail.com>
2025-04-15 21:51:46 +0200

    add migrations

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { migrate } from "drizzle-orm/libsql/migrator";
 
 const client = createClient({
   url: process.env.DATABASE_URL!,
@@ -12,4 +13,8 @@ const client = createClient({
 export const db = drizzle({
   client,
   logger: true,
+});
+
+await migrate(db, {
+  migrationsFolder: "./drizzle",
 });
